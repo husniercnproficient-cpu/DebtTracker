98e03d23a654c7a1fbcaaccda305a475e577eeb2
husni faruq <husni.ercnproficient@gmail.com>
2025-12-06 18:08:29 +0700

    Update project: ubah packgae.json, server.js

--- server.js
+++ server.js
@@ -64,6 +64,8 @@ app.delete('/api/debts/:nama', (req, res) => {
 });
 
 // Jalankan server
-app.listen(3000, () => {
-  console.log('Server running on http://localhost:3000');
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT, () => {
+  console.log('Server running on port ' + PORT);
 });
